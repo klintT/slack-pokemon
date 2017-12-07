6fcc8d323c49f84477b8f48e2453e142d08655ee
Kevin Ruffin <kruffin@gmail.com>
2017-12-07 16:12:46 -0500

    Fixes an issue where pm2 is dereferencing the link so new builds don't get started (just restarts the old code).

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 pkExist="`pm2 id \"Slack Pokemon\"`"
-if [ "$pkExist" = "[]" ]; then 
+if [ "$pkExist" = "[]" ]; then
     pm2 start index.js --name="Slack Pokemon"
 else 
+    # Remove the old one first
+    pm2 delete "Slack Pokemon"
     pm2 restart --update-env "Slack Pokemon"
 fi
