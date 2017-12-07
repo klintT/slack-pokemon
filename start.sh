ad3a57aeb0201f89841efe570100b3de664b0058
Kevin Ruffin <kruffin@gmail.com>
2017-12-07 00:45:22 -0500

    Pulling my hair... can't use == in bash...

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pkExist="`pm2 id \"Slack Pokemon\"`"
-if [ "$pkExist" == "[]" ]; then 
+if [ "$pkExist" = "[]" ]; then 
     pm2 start index.js --name="Slack Pokemon"
 else 
     pm2 restart --update-env "Slack Pokemon"
